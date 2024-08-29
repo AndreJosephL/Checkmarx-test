af49510ab9efb806846f009cacce1e3caadfaeec
André Joseph López Cortez <130710707+AndreJosephL@users.noreply.github.com>
2024-08-29 12:13:44 -0600

    Add files via upload

--- python-test.py
+++ python-test.py
@@ -1,1 +1,23 @@
+import collections
+import compileall
+import textwrap
+
 print("Hola mundo")
+notas = [67, 100, 87, 56]
+mi_lista = [1, 2, 3, 4, 5]
+
+nombre = "Nora"
+lenguaje_favorito = "Python"
+
+print(f"Hola, soy {nombre}. Estoy aprendiendo {lenguaje_favorito}.")
+
+# Tablero:
+# 0 = Espacio vacío
+# 1 = Moneda
+# 2 = Enemigo
+# 3 = Meta
+tablero = [[0, 0, 1],
+           [0, 2, 0],
+           [1, 0, 3]]
+
+mi_lista = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
